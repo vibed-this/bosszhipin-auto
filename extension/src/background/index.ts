bc3d15c127c9e21ea6b6e48722b13fb815267241
ickchan <pjskminori@gmail.com>
2026-07-06 12:35:53 +0800

    feat: Service Worker 保活机制（chrome.alarms 定期唤醒）

--- extension/src/background/index.ts
+++ extension/src/background/index.ts
@@ -1,6 +1,8 @@
-import { initSocket } from './socket';
+import { initSocket, getSocket } from './socket';
 import { setupTabListeners, pushSyncState } from './tab-manager';
 import { registerHandlers } from './handlers';
+
+const KEEPALIVE_ALARM = 'sw-keepalive';
 
 const socket = initSocket();
 setupTabListeners();
@@ -11,10 +13,26 @@ socket.on('connect', () => {
   pushSyncState();
 });
 
+// 定期唤醒 Service Worker，防止被 Chrome 销毁
+chrome.alarms.create(KEEPALIVE_ALARM, { periodInMinutes: 0.25 });
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === KEEPALIVE_ALARM) {
+    const s = getSocket();
+    if (!s.connected) {
+      s.connect();
+    }
+  }
+});
+
 chrome.runtime.onStartup.addListener(() => {
-  // Socket.IO 自动重连
+  chrome.alarms.create(KEEPALIVE_ALARM, { periodInMinutes: 0.25 });
 });
 
 chrome.runtime.onInstalled.addListener(() => {
-  // Socket.IO 自动重连
+  chrome.alarms.create(KEEPALIVE_ALARM, { periodInMinutes: 0.25 });
+});
+
+chrome.runtime.onSuspend.addListener(() => {
+  console.log('[BossRemote] Service Worker 即将休眠');
 });
